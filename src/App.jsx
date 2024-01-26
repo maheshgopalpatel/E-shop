ee2d1ba184cf0bb0e822a623266f84cf2216edd7
vivek kumar <vivekswami806@gmail.com>
2024-01-26 15:37:46 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import LoginPage from './component/LoginPage'
 
 function App() {
   return (
+    <>
     <div className=' text-2xl text-red-300'>App</div>
+    <LoginPage/>
+    </>
   )
 }
 
