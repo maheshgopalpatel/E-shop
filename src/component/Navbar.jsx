a6b5085c54f26d84b4fb9560a0260cf26c7d9fb6
Mahesh Patel <maheshgopalpatel@gmail.com>
2024-01-27 11:37:53 +0530

    updated

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import searchIcon from "../assets/image/search-icon.png"
+import searchButton from "../assets/image/icon.png"
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 const Navbar = () => {
   const navigation = [
     { name: "Home", href: "/", current: false },
-    { name: "About", href: "/", current: false },
     { name: "Product", href: "/", current: false },
     { name: "About", href: "/", current: false },
   ];
 
   return (
     <>
-      <nav>
+      <nav className=" flex justify-evenly items-center">
         <div>
-          <image src={""} art="" />
+          <img src={"http://www.hindigraphics.in/wp-content/uploads/2019/01/pro.png"} art="" className=" h-20"/>
         </div>
-        <div>
+        <div className="flex justify-between items-center">
           {navigation.map((item) => {
             return (
-              <div>
+              <div className="">
                 <Link
                   to={item.href}
                   className={classNames(
@@ -33,6 +37,11 @@ const Navbar = () => {
               </div>
             );
           })}
+        </div>
+        <div className=" relative ">
+          <input type="text" placeholder="Search Product.." value={""} onChange={(e)=>{searchHandler(e)}} className=" bg-transparent py-2 pl-10 pr-2 border-2 rounded-lg border-orange-400" />
+          <img src={searchIcon} alt="search-icon" className=" absolute h-8 top-2 left-1"/>
+          <button onClick={clickHandler} ><img src={searchButton} alt="search icon"/></button>
         </div>
       </nav>
     </>
